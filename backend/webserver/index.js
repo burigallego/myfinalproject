5a72e4a48572f39db885bce0f70571c7d57438b7
burigallego <iagogm_9@hotmail.com>
2019-04-03 20:18:17 +0200

    server working

---
+++ backend/webserver/index.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+//const routes = require('./routes');
+
+const app = express();
+let server = null;
+app.use(bodyParser.json());
+
+
+
+app.use((req, res, next) => {
+    const accessControlAllowMethods = [
+        // fill the methods
+    ];
+
+    const accessControlAllowHeaders = [
+        // fill the headers
+    ];
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    // Access-Control-Allow-Methods: put accessControlAllowHeaders separated by comma
+    res.header('Access-Control-Allow-Methods', accessControlAllowMethods.join(','));
+    // put accessControlAllowHeaders separated by comma
+    res.header('Access-Control-Allow-Headers', accessControlAllowHeaders.join(','));
+    next();
+});
+
+app.use('*', (req, res, next) => {
+    return res.status(404).send({
+        message: 'Route not found',
+    });
+});
+
+app.use((err, req, res, next) => {
+    if (err.name === 'ValidationError') {
+        return res.status(400).send(err);
+    }
+
+    if (err.name === 'AuthenticatedError') {
+        return res.status(401).send();
+    }
+
+    console.error('Error 500', err);
+    return res.status(500).send({
+        message: err.message,
+    });
+});
+
+async function listen(port) {
+    if (server === null) {
+        server = await app.listen(port);
+    } else {
+        console.error("Can't listen, server already initialized");
+    }
+}
+
+/**
+ * Stop listening requests
+ */
+async function close() {
+    if (server) {
+        await server.close();
+        server = null;
+    } else {
+        console.error("Can't close a non started server");
+    }
+}
+
+module.exports = {
+    listen,
+    close,
+};
+
