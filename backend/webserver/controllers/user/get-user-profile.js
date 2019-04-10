fd69e55b66057231e566c98ae533468bb9737f15
burigallego <iagogm_9@hotmail.com>
2019-04-10 23:46:53 +0200

    add get user backend

---
+++ backend/webserver/controllers/user/get-user-profile.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const mysqlPool = require('../../../databases/mysql-pool');
+
+async function getUserProfile(req, res, next) {
+    const { uuid } = req.claims;
+    const profileQuery = `SELECT uuid, full_name, address, tlf, avatar_url FROM users WHERE uuid='${uuid}'`;
+    try {
+        const connection = await mysqlPool.getConnection();
+        const [data] = await connection.query(profileQuery);
+        const [result] = data;
+        const profileData = {
+            uuid: result.uuid,
+            fullName: result.full_name,
+            address: result.address,
+            tlf: result.tlf,
+            avatarUrl: result.avatar_url,
+        };
+        return res.status(200).send(profileData);
+    } catch (e) {
+        return res.status(500).send(e.message);
+    }
+
+
+}
+
+module.exports = getUserProfile;
--- backend/webserver/routes/user-router.js
+++ backend/webserver/routes/user-router.js
@@ -4,7 +4,7 @@ const express = require('express');
 const multer = require('multer');
 
 
-
+const getUserProfile = require('../controllers/user/get-user-profile');
 const checkJwtToken = require('../controllers/session/check-jwt-token');
 const updateUserProfile = require('../controllers/user/update-user-profile');
 const uploadAvatar = require('../controllers/user/upload-avatar');
@@ -15,6 +15,8 @@ const upload = multer();
 
 router.put('/user', checkJwtToken, updateUserProfile);
 router.post('/user/avatar', checkJwtToken, upload.single('avatar'), uploadAvatar);
+router.get('/user', checkJwtToken, getUserProfile);
+
 
 
 module.exports = router;
