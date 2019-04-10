3744de29bd7dd2e40925646311bf25b38d9b156d
burigallego <iagogm_9@hotmail.com>
2019-04-10 23:08:42 +0200

    a ver si arreglo esto dunha vez

--- elearning/src/app/dashboard/components/dashboard/dashboard.component.ts
+++ elearning/src/app/dashboard/components/dashboard/dashboard.component.ts
@@ -1,20 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Store } from '@ngxs/store';
+import { GetUserProfile } from 'src/app/auth/store/auth.actions';
 
 @Component({
   selector: 'el-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, private store: Store) { }
 
+  ngOnInit() {
+    this.store.dispatch(new GetUserProfile());
+  }
 }
